4f66ee07ce5c1bc6a7893a656ccd789870426ce8
Bil-2 <biltubag29@gmail.com>
2026-01-03 12:29:10 +0530

    Add root route for backend verification

--- server/server.js
+++ server/server.js
@@ -85,16 +85,9 @@ app.use('/api/upload', uploadRoutes);
 const __dirname = path.resolve();
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 
-// if (process.env.NODE_ENV === 'production') {
-//   app.use(express.static(path.join(__dirname,'..', '/client/dist')));
-//   app.get('*', (req, res) =>
-//     res.sendFile(path.resolve(__dirname, '..', 'client', 'dist', 'index.html'))
-//   );
-// } else {
-//   app.get('/', (req, res) => {
-//     res.send('API is running...');
-//   });
-// }
+app.get('/', (req, res) => {
+  res.send('API is running...');
+});
 
 app.use(notFound);
 app.use(errorHandler);
