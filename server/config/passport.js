3d9958e5a01d4e45a30ab5f7b56ced7b7c9aca45
Bil-2 <biltubag29@gmail.com>
2026-02-23 16:15:59 +0530

    fix: hardcode netlify google callback url

--- server/config/passport.js
+++ server/config/passport.js
@@ -24,7 +24,7 @@ export const configurePassport = () => {
       {
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL: process.env.GOOGLE_CALLBACK_URL,
+        callbackURL: process.env.GOOGLE_CALLBACK_URL || 'https://hashnode-blogging-platform.netlify.app/api/auth/google/callback',
       },
       async (accessToken, refreshToken, profile, done) => {
         try {
