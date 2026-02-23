f416c8b8f67a8670997af831696a300088262260
Bil-2 <biltubag29@gmail.com>
2026-02-23 16:10:20 +0530

    fix: update netlify api wrapper routing

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -1,4 +1,18 @@
 import serverless from 'serverless-http';
 import app from '../../server/server.js';
 
-export const handler = serverless(app);
+const handlerOptions = {
+  request: (request, event, context) => {
+    let path = event.path;
+    if (path.startsWith('/.netlify/functions/api')) {
+      path = path.replace('/.netlify/functions/api', '');
+    }
+    if (path === '') path = '/';
+    if (!path.startsWith('/api')) {
+      path = `/api${path === '/' ? '' : path}`;
+    }
+    request.url = path;
+  }
+};
+
+export const handler = serverless(app, handlerOptions);
